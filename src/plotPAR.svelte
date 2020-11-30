bf31ac7cdfd1e03f09c0cf807d62daf409a80184
Toni Cafiero <tcafiero@hotmail.com>
2020-11-30 16:32:59 +0100

    work in progress

--- src/plotPAR.svelte
+++ src/plotPAR.svelte
@@ -5,6 +5,6 @@
 <section id="PlotSection" class="visible">
 <h1>PAR</h1>
 <div id="chart">
-<iframe width="300" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/4?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&title=PAR&type=line&xaxis=hh%3Amm&yaxis=PPFD"></iframe>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/4?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&title=PAR&type=line&xaxis=hh%3Amm&yaxis=PPFD"></iframe>
 </div>
 </section>
--- src/plotTemperature.svelte
+++ src/plotTemperature.svelte
@@ -5,7 +5,7 @@
 <section id="plotTemperature" class="visible">
 <h1>Temperature</h1>
 <div id="chart">
-<iframe width="400" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/1?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&title=Temperature&type=line&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/1?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&title=Temperature&type=line&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
 </div>
 </section>
 
