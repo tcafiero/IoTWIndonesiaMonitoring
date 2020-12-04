30ae75f4eb9b308d5c28182905b61482c4f96c23
Toni Cafiero <tcafiero@hotmail.com>
2020-12-04 10:39:07 +0100

    work in progress

--- src/plotHumidity.svelte
+++ src/plotHumidity.svelte
@@ -5,5 +5,6 @@
 <section id="PlotSection" class="visible">
 <h1>Humidity</h1>
 <div id="chart">
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/2?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&title=Humidity&type=line&xaxis=hh%3Amm&yaxis=%25"></iframe></div>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/167475/charts/2?bgcolor=%23ffffff&color=%23d62020&days=3&dynamic=true&max=100&min=0&title=Humidity&type=line&xaxis=hh%3Amm&yaxis=%25"></iframe>
+</div>
 </section>
